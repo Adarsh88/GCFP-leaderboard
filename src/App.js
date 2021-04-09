178ac5a3f826585e8c468a1232a04c3cc1af2be7
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-04-10 01:20:50 +0530

    suvarnesh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
+import CloudData from './data/google-data.json';
+import ProgressBar from './ProgressBar';
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchTerm, setSearchTerm] = useState('')
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }} className="App">
+            <input type="text" placeholder="Search" onChange={event => { setSearchTerm(event.target.value) }} />
+
+            {CloudData.filter((CloudDetail) => {
+                if (searchTerm == "") {
+                    return CloudDetail
+                } else if (CloudDetail.Student_Name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return CloudDetail
+                }
+            }).map((CloudDetail, index) => {
+                return <div className="card" index={index} >
+                    <div className="user-info">
+                        <h2>{CloudDetail.Student_Name}</h2>
+                        <ul class="badges">
+                            <li>{CloudDetail.Quests_Completed}<strong>Quests Completed</strong></li>
+                            <li>{CloudDetail.Skill_Badges_Completed}<strong>Skill Badges Completed</strong></li>
+                        </ul>                        
+                        <ProgressBar completed={CloudDetail.Quests_Completed} />
+
+
+                    </div>
+                </div>
+
+            })}
+
+        </div>
+    );
 }
 
 export default App;
